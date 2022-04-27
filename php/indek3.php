21ab29e464403558a87a38fa65f4f2f8d3ddd4f3
RamaAriya <91512892+RamaAriya@users.noreply.github.com>
2022-04-27 10:57:43 +0700

    Add files via upload

---
+++ php/indek3.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title> konversi tipe </title>
+        <body>
+            <?php
+            $a = 300.4;
+            echo $a;
+            echo "<br>";
+            echo "tipe Double : ", doubleval($a), "<br>";
+            echo "tipe Integer : ", intval($a), "<br>";
+            echo "tipe string : ", strval($a);
+            ?>
+            </body>
+    </head>
+    </html>
